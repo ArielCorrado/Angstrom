97f2f6e52b9953b5cbf5448a6643a6c14b95d704
Ariel Corrado <jarielc2012@yahoo.com.ar>
2023-11-05 18:34:34 -0300

    cm

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import Home from './pages/home/Home';
 import NavBar from './components/navBar/NavBar';
 import "./styles/generalStyles.css";
 import "./styles/animations.css";
+import { SpinnerContext } from './context/spinnerContext';
+import { useContext } from 'react';
 
 function App() {
+    const {spinner} = useContext(SpinnerContext);
+
     return (
         <BrowserRouter>
+            {spinner}
             <NavBar/>
             <Routes>
                 <Route path="/" element={<Home/>}/>     
