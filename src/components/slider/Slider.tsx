9d217f0788c6c197974893b8c68fcba03dd5fc0f
Ariel Corrado <jarielc2012@yahoo.com.ar>
2023-11-06 14:04:41 -0300

    cm

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -6,15 +6,15 @@ function Slider() {
             <div className="sliderTextsCont sliderTextsContIzq flex column"></div>
             <div className="sliderImgsCont">
                 <img src="/images/blueprints/bp4.png" alt="Angstrom blueprint" className='sliderImg1 sliderImg1_AnimationOnEnter'/>
-                <img src="/images/home/3b.jpg" alt="Angstrom team" className='sliderImg2 sliderImg2_AnimationOnEnter'/>
+                <img src="/images/home/3b.jpg" alt="Angstrom team" className='sliderImg2 opacityOnCharge animation_duration250'/>
                 <img src="/images/logos/compas.png" alt="Angstrom compas" className="sliderImgCompas sliderImgCompas_AnimationOnEnter"/>
             </div>
             <div className="sliderTextsCont flex column"> 
-                <div className="sliderTextsCont2 sliderTextsCont2_AnimationOnEnter flex column">
-                    <p className="sliderText1">Angström</p>
-                    <p className="sliderText2">Técnica</p>
-                    <p className="sliderText3">Somos una consultora de <br />documentación técnica <br />de la industria de la <br />construcción</p>
-                    <button className="sliderButton flex">Conocer más</button>
+                <div className="sliderTextsCont2 flex column">
+                    <p className="sliderText1 sliderTexts_AnimationOnEnter">Angström</p>
+                    <p className="sliderText2 sliderTexts_AnimationOnEnter animation_delay025">Técnica</p>
+                    <p className="sliderText3 sliderTexts_AnimationOnEnter animation_delay050">Somos una consultora de <br />documentación técnica <br />de la industria de la <br />construcción</p>
+                    <button className="sliderButton sliderTexts_AnimationOnEnter animation_delay075 flex">Conocer más</button>
                 </div>
             </div>
         </div>
