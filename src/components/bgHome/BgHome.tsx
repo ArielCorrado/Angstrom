aa112913b09d3f6a585cbf7c36b41794214789eb
Ariel Corrado <jarielc2012@yahoo.com.ar>
2023-11-06 02:33:55 -0300

    cm

--- src/components/bgHome/BgHome.tsx
+++ src/components/bgHome/BgHome.tsx
@@ -3,13 +3,118 @@ import "./bgHome.css"
 function BgHome() {
     return (
         <div className="bgHomeCont flex">
-            <div className="bgHomeDiv"></div>
-            <div className="bgHomeDiv"></div>
-            <div className="bgHomeDiv"></div>
-            <div className="bgHomeDiv"></div>
-            <div className="bgHomeDiv"></div>
-            <div className="bgHomeDiv"></div>
-            <div className="bgHomeDiv"></div>
+            <div className="bgHomeDiv flex column">
+                <div className="bgHomeDivsCont1 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+                <div className="bgHomeDivsCont2 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+            </div>
+            <div className="bgHomeDiv flex column">
+                <div className="bgHomeDivsCont1 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+                <div className="bgHomeDivsCont2 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+            </div>
+            <div className="bgHomeDiv flex column">
+                <div className="bgHomeDivsCont1 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+                <div className="bgHomeDivsCont2 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+            </div>
+            <div className="bgHomeDiv flex column">
+                <div className="bgHomeDivsCont1 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+                <div className="bgHomeDivsCont2 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+            </div>
+            <div className="bgHomeDiv flex column">
+                <div className="bgHomeDivsCont1 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+                <div className="bgHomeDivsCont2 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+            </div>
+            <div className="bgHomeDiv flex column">
+                <div className="bgHomeDivsCont1 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+                <div className="bgHomeDivsCont2 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+            </div>
+            <div className="bgHomeDiv flex column">
+                <div className="bgHomeDivsCont1 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+                <div className="bgHomeDivsCont2 flex">
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                    <div className="bgHomeDiv2a"></div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -3,17 +3,17 @@ import "./slider.css";
 function Slider() {
     return (
         <div className="sliderHome flex">
-            <div className="sliderTextsCont flex column"></div>
-            <div className="sliderImgsAndTextCont">
+            <div className="sliderTextsCont sliderTextsContIzq flex column"></div>
+            <div className="sliderImgsCont">
                 <img src="/images/blueprints/bp4.png" alt="Angstrom blueprint" className='sliderImg1 sliderImg1_AnimationOnEnter'/>
                 <img src="/images/home/3b.jpg" alt="Angstrom team" className='sliderImg2 sliderImg2_AnimationOnEnter'/>
                 <img src="/images/logos/compas.png" alt="Angstrom compas" className="sliderImgCompas sliderImgCompas_AnimationOnEnter"/>
             </div>
             <div className="sliderTextsCont flex column"> 
                 <div className="sliderTextsCont2 sliderTextsCont2_AnimationOnEnter flex column">
-                    <p className="sliderText1">Angstrom</p>
+                    <p className="sliderText1">Angström</p>
                     <p className="sliderText2">Técnica</p>
-                    <p className="sliderText3">Somos una consultora de <br />documentacion técnica <br />de la industria de la <br />construcción</p>
+                    <p className="sliderText3">Somos una consultora de <br />documentación técnica <br />de la industria de la <br />construcción</p>
                     <button className="sliderButton flex">Conocer más</button>
                 </div>
             </div>
