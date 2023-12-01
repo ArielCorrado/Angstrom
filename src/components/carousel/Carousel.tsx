4009b1771a9667d6594b2d74fc5da92e1d03414f
ArielCorrado <jarielc2012@yahoo.com.ar>
2023-12-01 15:40:58 -0300

    cm

---
+++ src/components/carousel/Carousel.tsx
@@ -0,0 +1,98 @@
+import React, { useState, useEffect, useRef } from 'react';
+import "./carousel.css";
+
+const Carousel = (props: {imgsRoutes: string[], imgSelectPos: string, imgClass: string, closeFunction: () => void}) => {
+    
+    const [imgSrc, setImgSrc] = useState <string> (props.imgsRoutes[parseInt(props.imgSelectPos)]);
+    const [imgPos, setimgPos] = useState <number> (parseInt(props.imgSelectPos));
+    const imgPosRef = useRef(imgPos);
+   
+    useEffect(() => {
+                            
+        /*********************************************************************/
+        let startX: number;
+        let startY: number;
+        let endX: number;
+        let endY: number;
+        const elemento: HTMLElement | null = document.querySelector(".seccions");
+        const start = (e: TouchEvent) => {
+            startX = e.touches[0].clientX;
+            startY = e.touches[0].clientY;
+        }
+
+        const end = (e: TouchEvent) => {
+            endX = e.changedTouches[0].clientX;
+            endY = e.changedTouches[0].clientY;
+            const Ax = endX - startX;
+            const Ay = Math.abs(endY - startY);
+            if (Ax > 50 && Ay < 100) {
+                nextImage(false);
+                // console.log("prev")
+            } else if (Ax < -50 && Ay < 100) {
+                nextImage(true);
+                // console.log("next")
+            }
+        }
+
+        elemento?.addEventListener("touchstart", start);
+        elemento?.addEventListener("touchend", end);
+
+        /*********************************************************************/
+        
+        // eslint-disable-next-line
+    }, []);
+
+    useEffect(() => {
+        if (typeof imgPos === "number") {
+            setImgSrc(props.imgsRoutes[imgPos]);
+            imgPosRef.current = imgPos;
+
+            console.log(imgPos)
+            console.log(imgSrc)
+        }
+    }, [imgPos])   
+
+    const nextImage = (opc: boolean) => {
+        if (opc) {
+            if (imgPosRef.current === props.imgsRoutes.length - 1) {
+                setimgPos(0);
+                console.log("set")
+            } else {
+                setimgPos(imgPosRef.current + 1);
+                console.log("set")
+            }  
+        } else {
+            if (imgPosRef.current === 0) {
+                setimgPos(props.imgsRoutes.length - 1);
+                console.log("set")
+            }  else {
+                setimgPos(imgPosRef.current - 1);
+                console.log("set")
+            }
+        }
+    }    
+    
+    useEffect(() => {
+        if (imgSrc) {
+            const img = document.querySelector(".carouselActualImg") as HTMLImageElement;
+            img.classList.remove("opacityOnCharge");
+            setTimeout(() => {
+                img.classList.add("opacityOnCharge");
+            }, 100);
+        }
+        // console.log(imgSrc)
+        // console.log(imgPos)
+        //eslint-disable-next-line
+    }, [imgSrc]);
+             
+    return (
+        <div className='carouselCont flex'>
+            <img src="/images/icons/close.png" className='cvCloseIcon carouselCloseIcon' alt="Exit Icon" onClick={() => {props.closeFunction(); document.body.style.overflow = "initial"}}/>
+            <img src="/images/icons/next.png" className='carouselNextIcon' alt="Next Icon" onClick={() => nextImage(true)}/>
+            <img src="/images/icons/next.png" className='carouselPrevIcon' alt="Prev Icon" onClick={() => nextImage(false)}/>
+            <img src={imgSrc!} className={`carouselActualImg ${props.imgClass}`} alt="Carousel Img" />
+        </div>        
+    );
+}
+
+export default Carousel;
--- src/components/homePhotos1/HomePhotos1.tsx
+++ src/components/homePhotos1/HomePhotos1.tsx
@@ -1,18 +1,60 @@
 import "./homePhotos1.css"
 import CardImg1 from "../../cards/cardImg1/CardImg1"
+import Carousel from "../carousel/Carousel"
+import { useState, useEffect } from "react"
 
 function HomePhotos1() {
+    
+    const [carousel, setCarousel] = useState <JSX.Element | null> (null);
+    
+    useEffect(() => {
+
+        const seccionZindexMax = (opc: boolean) => {
+            const seccion = document.querySelector(".seccions") as HTMLDivElement;
+            opc ?  seccion!.classList.add("zindex1100") : seccion!.classList.remove("zindex1100");
+        }
+
+        const showCarousel = (e: Event) => {
+            const cardsImgs : NodeListOf<HTMLImageElement> = document.querySelectorAll(".homeCardImg1Img");
+            const imgsRoutes : string[] = [];
+            cardsImgs.forEach((card) => {
+                imgsRoutes.push(card.src);
+            });
+            const imgSelect = e.target as HTMLDivElement;
+            const imgSelectPos = imgSelect.getAttribute("pos");
+            setCarousel(<Carousel imgsRoutes={imgsRoutes} imgSelectPos={imgSelectPos!} imgClass="g1ImgsAdjust" closeFunction={() => {setCarousel(null); seccionZindexMax(false)}}/>);
+            document.body.style.overflow = "hidden";
+        }
+        
+        const addListenners = () => {
+            const cardsImgs = document.querySelectorAll(".homeCardImg1Cont");
+            cardsImgs.forEach((card, index) => {
+                card.setAttribute("pos", index.toString());
+                card.addEventListener("click", (e: Event) => {
+                    seccionZindexMax(true);
+                    showCarousel(e);
+                })
+            })
+        }
+
+        addListenners();
+                
+    }, []);
+
     return (
-        <div className="homePhotos1Cont flex wrap">
-            <CardImg1 imgSrc="/images/design/1.jpeg" text="Texto 1" />
-            <CardImg1 imgSrc="/images/design/2.jpeg" text="Texto 2" />
-            <CardImg1 imgSrc="/images/design/3.jpeg" text="Texto 3" />
-            <CardImg1 imgSrc="/images/design/4.jpeg" text="Texto 4" />
-            <CardImg1 imgSrc="/images/design/5.jpeg" text="Texto 5" />
-            <CardImg1 imgSrc="/images/design/6.jpeg" text="Texto 6" />
-            <CardImg1 imgSrc="/images/design/7.jpeg" text="Texto 7" />
-            <CardImg1 imgSrc="/images/design/8.jpeg" text="Texto 8" />
-        </div>
+        <>
+            {carousel}
+            <div className="homePhotos1Cont flex wrap">
+                <CardImg1 imgSrc="/images/design/1.jpeg" text="Texto 1" />
+                <CardImg1 imgSrc="/images/design/2.jpeg" text="Texto 2" />
+                <CardImg1 imgSrc="/images/design/3.jpeg" text="Texto 3" />
+                <CardImg1 imgSrc="/images/design/4.jpeg" text="Texto 4" />
+                <CardImg1 imgSrc="/images/design/5.jpeg" text="Texto 5" />
+                <CardImg1 imgSrc="/images/design/6.jpeg" text="Texto 6" />
+                <CardImg1 imgSrc="/images/design/7.jpeg" text="Texto 7" />
+                <CardImg1 imgSrc="/images/design/8.jpeg" text="Texto 8" />
+            </div>
+        </>
     )
 }
 
