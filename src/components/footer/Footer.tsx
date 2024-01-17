95ccee6c5337ddba928176b6b79143adb53a80d6
ArielCorrado <jarielc2012@yahoo.com.ar>
2024-01-17 10:57:43 -0300

    cm

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./footer.css";
 function Footer() {
     return (
         <div className="footerBottomCont flex">
-            <p className="footerCopy">© 2023 Angstrom técnica, All Rights Reserved</p>
+            <p className="footerCopy">© 2024 Angstrom técnica, All Rights Reserved</p>
         </div>
     )
 }
